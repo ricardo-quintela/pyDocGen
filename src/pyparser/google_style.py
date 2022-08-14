52a730e680ae684e1e663bab118b25e324af685d
Ricardo Quintela <69524535+ricardo-quintela@users.noreply.github.com>
2022-08-15 00:58:24 +0100

    changed header in the function/method display from ### to ##

--- src/pyparser/google_style.py
+++ src/pyparser/google_style.py
@@ -261,7 +261,7 @@ def google_doc_parser(path: str):
         with open(md_file_path, "a") as file:
 
             # write the name and the description
-            append_file(file, "### " + re.sub(r"_", "\\_", name) + "  \n  \n**Signature:**  \n  \n>" + chr(96)*3 + re.sub(r"\n+", "", signature) + chr(96)*3 + "  \n  \n**Description:**  \n  \n>" + re.sub(r"\\n+", "  \n>", re.sub(r" {2,}", "", re.sub(r"\n+", "", description))) + "  \n  \n")
+            append_file(file, "## " + re.sub(r"_", "\\_", name) + "  \n  \n**Signature:**  \n  \n>" + chr(96)*3 + re.sub(r"\n+", "", signature) + chr(96)*3 + "  \n  \n**Description:**  \n  \n>" + re.sub(r"\\n+", "  \n>", re.sub(r" {2,}", "", re.sub(r"\n+", "", description))) + "  \n  \n")
             
             # write the args if they exist
             if docstring_start != 2 and args_ind >= len("Args:\n"):
